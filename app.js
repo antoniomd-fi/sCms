b7c72f9b8242c117889bd6d88e2d9052f9f07e43
VictorSosa-dev <manuelsosapia@gmail.com>
2022-10-06 01:03:58 -0500

    feat: autogenerete documentation swagger

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const sequelize = require('./config/db.js');
 const express = require('express');
+const swaggerUi = require('swagger-ui-express')
+const sequelize = require('./config/db.js');
+const swaggerFile = require('./swagger_output.json')
 const routes = require('./routes/index.routes.js');
-const { swaggerDocs } = require('./swagger.js');
 
 const app = express();
 app.use(express.json());
 
 // Routes
 app.use('/', routes);
+app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 try {
     sequelize.authenticate();
@@ -20,5 +22,4 @@ try {
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log("Server listing on PORT 3000");
-    swaggerDocs(app, PORT);
 });
