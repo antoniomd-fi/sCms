70c2bb9b182c031a13d462b23f2fa73a1de16892
Antonio Martin <90221784+antoniomd-fi@users.noreply.github.com>
2022-10-06 11:47:08 -0500

    Feat: User Beta model

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,1 +1,69 @@
+const User = require('../models/users.js');
 
+async function create(req, res) {
+    // Validate request
+    if (!req.body.username || !req.body.name || !req.body.surname || !req.body.email || !req.body.phone || !req.body.isAdmin || !req.body.password_hash || !req.body.password_salt) {
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
+        return;
+    }
+
+    // Create a User
+    const user = {
+        username: req.body.username,
+        name: req.body.name,
+        surname: req.body.surname,
+        email: req.body.email,
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        password_hash: req.body.password_hash,
+        password_salt: req.bo.password_salt
+    };
+
+    // Save User in the database
+    try {
+        const data = await User.create(user);
+        res.send(data);
+    } catch (error) {
+        res.status(500).send({
+            message: error.message || "Some error occurred while creating the Student."
+        });
+    }
+}
+
+async function signUp(req, res) {
+    const body = req.body;
+    try { 
+        const user = await User.create(body);
+        const {salt, hash} = User.createPassword(body['password']);
+        user.password_salt = salt;
+        user.password_hash = hash;
+        await user.save();
+        res.status(201).json(user);
+    } catch (err) {
+        if (["SequelizeValidationError", "SequelizeUniqueConstraintError"].includes(err.name) ) {
+            return res.status(400).json({
+                error: err.errors.map(e => e.message)
+            })
+        }
+        else {
+            throw err;
+        }
+    }
+}
+
+async function logIn(req, res) {
+    const body = req.body;
+    const user = await User.findOne({where: {username: body['username']}});
+    if (!user) {
+        return res.status(404).json({error: "User not found"});
+    }
+    if (User.validatePassword(body['password'], user.password_salt, user.password_hash)) {
+        return res.status(200).json({mensaje: "Bienvenido!"});
+    } else {
+        return res.status(400).json({mensaje: "Password Incorrecto"});
+    }
+}
+
+module.exports = { signUp, logIn }
--- models/users.js
+++ models/users.js
@@ -1,1 +1,68 @@
 
+const crypto = require('crypto');
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const User = sequelize.define('User', {
+    username: {
+        type: DataTypes.CHAR(64),
+        allowNull: false,
+        unique: true,
+        validate: {
+            is: /^[a-zA-Z0-9_-]+$/
+        }
+    },
+    name: {
+        type: DataTypes.CHAR(64),
+        allowNull: false
+    },
+    surname: {
+        type: DataTypes.CHAR(128),
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.CHAR(64),
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    phone:{
+        type: DataTypes.CHAR(10),
+        allowNull: false,
+        validate:{
+            is: /^\+[0-9][0-9]\d{10}$/
+        }
+    },
+    isAdmin: {
+        type: Boolean,
+        allowNull: false,
+    },
+    password_hash: {
+        type: DataTypes.CHAR(64),
+        allowNull: true,
+    },
+    password_salt: {
+        type: DataTypes.CHAR(64),
+        allowNull: true,
+    },
+
+});
+
+User.createPassword = function(plainText) {
+    const salt = crypto.randomBytes(16).toString('hex');
+    const hash = crypto
+        .pbkdf2Sync(plainText, salt, 10000, 512, "sha512")
+        .toString("hex");
+    return {salt: salt, hash: hash}
+}
+
+User.validatePassword = function(password, user_salt, user_hash) {
+    const hash = crypto
+        .pbkdf2Sync(password, user_salt, 10000, 512, "sha512")
+        .toString("hex");
+    return user_hash === hash;
+}
+
+module.exports = User;
